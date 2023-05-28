40ebdffe44325e79c7ca5bf712466a147a49fa3e
Taiwo <121824577+Taiwoademola@users.noreply.github.com>
2023-05-28 19:49:16 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,3 +1,12 @@
+#include "main.h"
+
+/**
+ * is_printable - Evaluates if a char is printable
+ * Done by Jemilat and Doris.
+ * @c: Char to be evaluated.
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
