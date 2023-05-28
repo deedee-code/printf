66c2aea0c60ea1de11f25e2ac11eea556453d288
Taiwo <121824577+Taiwoademola@users.noreply.github.com>
2023-05-28 19:39:01 +0100

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -1,3 +1,18 @@
+#include "main.h"
+
+/************************* PRINT CHAR *************************/
+
+/**
+ * print_char - Prints a char
+ * Done by Jemilat and Doris
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
