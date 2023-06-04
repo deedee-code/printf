8c24c46417a3d73d5983aee8cd91ae1cbdc99165
Taiwo <121824577+Taiwoademola@users.noreply.github.com>
2023-06-04 23:46:50 +0100

    Update flags.c

--- flags.c
+++ flags.c
@@ -1,11 +1,3 @@
-#include "main.h"
-/**
- * get_flags - Calculates active flags
- * by Jemilat and Doris
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
- */
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
